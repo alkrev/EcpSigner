d0d2713c755d710b58e8a9bcbcf2e6c2ab36eaa2
Krylov Alexander <alkrev@gmail.com>
2025-04-14 10:57:25 +0300

    Added cleaning of cache from expired values

--- EcpSigner/cache/cache.cs
+++ EcpSigner/cache/cache.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CacheTools
 {
@@ -12,37 +13,39 @@ namespace CacheTools
             this.minutes = minutes;
             cache = new Dictionary<string, DateTime>();
         }
-
         public void SetRange(List<string> numbers)
         {
-            DateTime t = DateTime.UtcNow.AddMinutes(minutes);
+            DateTime now = DateTime.UtcNow.AddMinutes(minutes);
             foreach (string num in numbers)
             {
-                cache[num] = t;
+                cache[num] = now;
             }
         }
-
         public bool Contains(string number)
         {
-            if (cache.ContainsKey(number)) {
-                if (cache[number] < DateTime.UtcNow)
+            if (cache.TryGetValue(number, out DateTime expirationTime))
+            {
+                if (expirationTime < DateTime.UtcNow)
                 {
                     cache.Remove(number);
                     return false;
                 }
-                else
-                {
-                    return true;
-                }
-            } 
-            else
-            {
-                return false;
+                return true;
             }
+            return false;
         }
         public int Count()
         {
             return cache.Count;
+        }
+        public void RemoveExpired()
+        {
+            DateTime now = DateTime.UtcNow;
+            var expiredKeys = cache.Where(entry => entry.Value < now).Select(entry => entry.Key).ToList();
+            foreach (string key in expiredKeys)
+            {
+                cache.Remove(key);
+            }
         }
     }
 }
