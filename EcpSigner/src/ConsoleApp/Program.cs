e8d0e9b3604a2cad05bc91846cd349e67ce854ba
Krylov Alexander <alkrev@gmail.com>
2025-05-16 13:09:50 +0300

    careful logging shutdown added

--- EcpSigner/src/ConsoleApp/Program.cs
+++ EcpSigner/src/ConsoleApp/Program.cs
@@ -9,6 +9,7 @@ using EcpSigner.Infrastructure.Decorators;
 using EcpSigner.Infrastructure.Repositories;
 using EcpSigner.Infrastructure.WebClients;
 using EcpSigner.Infrastructure.Workers;
+using NLog;
 using Portal;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -23,30 +24,37 @@ namespace EcpSigner
         /// </summary>
         static void Main(string[] args)
         {
-            // Собираем DI
-            // Инфраструктура с использованием библиотек
-            var logger = new NLogLogger(NLog.LogManager.GetLogger("EcpSigner"));
-            var config = new JsonConfigurationProvider(logger, "config.json");
-            var webClient = new WebClient(new WebTools.Client(config.Get().url));
-            var portalServiceDecorator = new PortalServiceDecorator(new PortalService(new Main(webClient), new EMD(webClient) ), logger);
-            var crypto = new CryptographyTools.Signing.CryptoPro.Crypto();
-            var store = new CryptographyTools.Store.CurrentUserStore();
-            var signatureServiceDecorator = new SignatureServiceDecorator(new SignatureService(crypto, store), logger);
-            var cache = new Cache(new CacheTools.Cache(config.Get().cacheMinutes));
-            var dates = new DatesService(args);
-            var flashWindowService = new FlashWindowService(new FlashWindow(Process.GetCurrentProcess().MainWindowHandle));
-            // Бизнес-логика (приложение)
-            var signDocumentWorflow = new SignDocumentWorflow(portalServiceDecorator, signatureServiceDecorator, logger);
-            var signDocumentsLoop = new SignDocumentsLoop(logger, config, signDocumentWorflow);
-            var prepareSigningWorkflow = new PrepareSigningWorkflow(portalServiceDecorator, signatureServiceDecorator, logger, config, dates, cache, flashWindowService, signDocumentsLoop);
-            var documentSigningJob = new DocumentSigningJob(prepareSigningWorkflow, logger);
-            // Название программы
-            new AppTitleService(logger).Set();
-            // Источник остановки работы
-            var source = new ConsoleCtrlCCancellationTokenSource();
-            // Запуск
-            var worker = new DocumentSigningWorker(documentSigningJob, logger, config);
-            Task.Run(async () => await worker.Run(source.Token)).Wait();
+            try
+            {
+                // Собираем DI
+                // Инфраструктура с использованием библиотек
+                var logger = new NLogLogger(LogManager.GetLogger("EcpSigner"));
+                var config = new JsonConfigurationProvider(logger, "config.json");
+                var webClient = new WebClient(new WebTools.Client(config.Get().url));
+                var portalServiceDecorator = new PortalServiceDecorator(new PortalService(new Main(webClient), new EMD(webClient)), logger);
+                var crypto = new CryptographyTools.Signing.CryptoPro.Crypto();
+                var store = new CryptographyTools.Store.CurrentUserStore();
+                var signatureServiceDecorator = new SignatureServiceDecorator(new SignatureService(crypto, store), logger);
+                var cache = new Cache(new CacheTools.Cache(config.Get().cacheMinutes));
+                var dates = new DatesService(args);
+                var flashWindowService = new FlashWindowService(new FlashWindow(Process.GetCurrentProcess().MainWindowHandle));
+                // Бизнес-логика (приложение)
+                var signDocumentWorflow = new SignDocumentWorflow(portalServiceDecorator, signatureServiceDecorator, logger);
+                var signDocumentsLoop = new SignDocumentsLoop(logger, config, signDocumentWorflow);
+                var prepareSigningWorkflow = new PrepareSigningWorkflow(portalServiceDecorator, signatureServiceDecorator, logger, config, dates, cache, flashWindowService, signDocumentsLoop);
+                var documentSigningJob = new DocumentSigningJob(prepareSigningWorkflow, logger);
+                // Название программы
+                new AppTitleService(logger).Set();
+                // Источник остановки работы
+                var source = new ConsoleCtrlCCancellationTokenSource();
+                // Запуск
+                var worker = new DocumentSigningWorker(documentSigningJob, logger, config);
+                Task.Run(async () => await worker.Run(source.Token)).Wait();
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
         }
     }
 }
