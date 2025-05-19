0cf4d07b82674b4357c666463c00479adc92ba3e
Krylov Alexander <alkrev@gmail.com>
2025-05-19 13:46:12 +0300

    interface implemented

---
+++ EcpSigner/src/Infrastructure/ConsoleCtrlCCancellationService.cs
@@ -0,0 +1,37 @@
+﻿using EcpSigner.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EcpSigner.Infrastructure
+{
+    class ConsoleCtrlCCancellationService: IConsoleControlService
+    {
+        /** 
+        * Обрабатываем Ctrl-C
+        */
+        private void CancelKeyPressHandler(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            cancelTokenSource.Cancel();
+        }
+
+        private readonly CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+        private bool _disposed;
+        public void StartListening()
+        {
+            Console.CancelKeyPress += CancelKeyPressHandler;
+        }
+
+        public CancellationTokenSource GetCancellationTokenSource() => cancelTokenSource;
+        public void Dispose()
+        {
+            if (_disposed) return;
+            Console.CancelKeyPress -= CancelKeyPressHandler;
+            _disposed = true;
+        }
+    }
+}
