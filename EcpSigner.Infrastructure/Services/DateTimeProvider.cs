008cc6521eaa74b1ff17e2f827eaa3a91f7325b6
Krylov Alexander <alkrev@gmail.com>
2025-05-27 16:19:43 +0300

    DateProvider implementation added

---
+++ EcpSigner.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,15 @@
+﻿using EcpSigner.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcpSigner.Infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
