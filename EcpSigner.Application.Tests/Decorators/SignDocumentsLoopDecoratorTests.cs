f4991de5caa1bd5c4e63cf54444886956550b511
Krylov Alexander <alkrev@gmail.com>
2025-05-27 08:51:56 +0300

    application tests added

---
+++ EcpSigner.Application.Tests/Decorators/SignDocumentsLoopDecoratorTests.cs
@@ -0,0 +1,90 @@
+﻿using EcpSigner.Application.Interfaces;
+using EcpSigner.Domain.Interfaces;
+using EcpSigner.Domain.Models;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcpSigner.Application.Decorators
+{
+    public class SignDocumentsLoopDecoratorTests
+    {
+        private readonly Mock<ISignDocumentsLoop> _innerMock;
+        private readonly Mock<ICacheService> _cacheMock;
+        private readonly Mock<ILogger> _loggerMock;
+        private readonly SignDocumentsLoopDecorator _decorator;
+
+        public SignDocumentsLoopDecoratorTests()
+        {
+            _innerMock = new Mock<ISignDocumentsLoop>();
+            _cacheMock = new Mock<ICacheService>();
+            _loggerMock = new Mock<ILogger>();
+
+            _decorator = new SignDocumentsLoopDecorator(
+                _innerMock.Object,
+                _cacheMock.Object,
+                _loggerMock.Object,
+                Mock.Of<IDelayProvider>()
+            );
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_Call_Inner_RunAsync_And_Return_Result()
+        {
+            // Arrange
+            var docs = new List<Document> { new Document() };
+            var certs = new List<(EcpCertificate, ICertificate)>();
+            var expectedResult = (signedCount: 2, docsToCache: new List<string> { "doc1", "doc2" });
+
+            _innerMock
+                .Setup(x => x.RunAsync(docs, certs, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(expectedResult);
+
+            // Act
+            var result = await _decorator.RunAsync(docs, certs, CancellationToken.None);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedResult);
+            _innerMock.Verify(x => x.RunAsync(docs, certs, It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_Cache_DocsToCache()
+        {
+            // Arrange
+            var expectedDocsToCache = new List<string> { "doc1", "doc2" };
+            _innerMock
+                .Setup(x => x.RunAsync(It.IsAny<List<Document>>(), It.IsAny<List<(EcpCertificate, ICertificate)>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync((2, expectedDocsToCache));
+
+            // Act
+            await _decorator.RunAsync(new List<Document>(), new List<(EcpCertificate, ICertificate)>(), CancellationToken.None);
+
+            // Assert
+            _cacheMock.Verify(c => c.SetRange(expectedDocsToCache), Times.Once);
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_Log_Info_Messages()
+        {
+            // Arrange
+            var docsToCache = new List<string> { "doc1" };
+            _innerMock
+                .Setup(x => x.RunAsync(It.IsAny<List<Document>>(), It.IsAny<List<(EcpCertificate, ICertificate)>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync((1, docsToCache));
+
+            _cacheMock.Setup(c => c.Count()).Returns(1);
+
+            // Act
+            await _decorator.RunAsync(new List<Document>(), new List<(EcpCertificate, ICertificate)>(), CancellationToken.None);
+
+            // Assert
+            _loggerMock.Verify(l => l.Info(It.Is<string>(msg => msg.Contains("подписываем документы"))), Times.Once);
+            _loggerMock.Verify(l => l.Info(It.Is<string>(msg => msg.Contains("подписано документов [1]"))), Times.Once);
+        }
+    }
+}
---
+++ EcpSigner.Application.Tests/Jobs/DocumentSigningJobTests.cs
@@ -0,0 +1,136 @@
+﻿using EcpSigner.Application.Interfaces;
+using EcpSigner.Domain.Exceptions;
+using EcpSigner.Domain.Interfaces;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcpSigner.Application.Jobs
+{
+    public class DocumentSigningJobTests
+    {
+        private readonly Mock<IJob> _mockWorkflow;
+        private readonly Mock<ILogger> _mockLogger;
+        private readonly DocumentSigningJob _job;
+
+        public DocumentSigningJobTests()
+        {
+            _mockWorkflow = new Mock<IJob>();
+            _mockLogger = new Mock<ILogger>();
+            _job = new DocumentSigningJob(_mockWorkflow.Object, _mockLogger.Object);
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_CompleteSuccessfully_When_NoExceptions()
+        {
+            // Act
+            Func<Task> act = async () => await _job.RunAsync(CancellationToken.None);
+
+            // Assert
+            await act.Should().NotThrowAsync();
+            _mockWorkflow.Verify(x => x.RunAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_LogFatalAndThrow_When_BreakWorkExceptionThrown()
+        {
+            // Arrange
+            _mockWorkflow
+                .Setup(x => x.RunAsync(It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new BreakWorkException("breaking"));
+
+            // Act
+            Func<Task> act = async () => await _job.RunAsync(CancellationToken.None);
+
+            // Assert
+            await act.Should().ThrowAsync<Exception>()
+                .WithMessage("DocumentSigningJob: breaking");
+
+            _mockLogger.Verify(x => x.Fatal("DocumentSigningJob: breaking"), Times.Once);
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_LogInfo_When_StopWorkExceptionThrown()
+        {
+            // Arrange
+            _mockWorkflow
+                .Setup(x => x.RunAsync(It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new StopWorkException());
+
+            // Act
+            await _job.RunAsync(CancellationToken.None);
+
+            // Assert
+            _mockLogger.Verify(x => x.Info("остановка работы"), Times.Once);
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_LogWarn_When_IsNotLoggedInExceptionThrown()
+        {
+            // Arrange
+            _mockWorkflow
+                .Setup(x => x.RunAsync(It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new IsNotLoggedInException("вход не выполнен"));
+
+            // Act
+            await _job.RunAsync(CancellationToken.None);
+
+            // Assert
+            _mockLogger.Verify(x => x.Warn("вход не выполнен"), Times.Once);
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_LogDebug_When_ContinueExceptionThrown()
+        {
+            // Arrange
+            var message = "continue";
+            _mockWorkflow
+                .Setup(x => x.RunAsync(It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new ContinueException(message));
+
+            // Act
+            await _job.RunAsync(CancellationToken.None);
+
+            // Assert
+            _mockLogger.Verify(x => x.Debug(message), Times.Once);
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_LogError_When_ContinueExceptionWithErrorThrown()
+        {
+            // Arrange
+            var message = "error occurred";
+            _mockWorkflow
+                .Setup(x => x.RunAsync(It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new ContinueExceptionWithError(message));
+
+            // Act
+            await _job.RunAsync(CancellationToken.None);
+
+            // Assert
+            _mockLogger.Verify(x => x.Error($"DocumentSigningJob: {message}"), Times.Once);
+        }
+
+        [Fact]
+        public async Task RunAsync_Should_LogErrorAndDebug_When_UnexpectedExceptionThrown()
+        {
+            // Arrange
+            var exception = new InvalidOperationException("unexpected");
+            _mockWorkflow
+                .Setup(x => x.RunAsync(It.IsAny<CancellationToken>()))
+                .ThrowsAsync(exception);
+
+            // Act
+            await _job.RunAsync(CancellationToken.None);
+
+            // Assert
+            _mockLogger.Verify(x => x.Error("DocumentSigningJob: unexpected"), Times.Once);
+            _mockLogger.Verify(x => x.Debug($"DocumentSigningJob: {exception.GetType().Name}"), Times.Once);
+            _mockLogger.Verify(x => x.Debug(It.Is<string>(s => s.StartsWith("DocumentSigningJob:"))), Times.AtLeast(1));
+        }
+    }
+}
