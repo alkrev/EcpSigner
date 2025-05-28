0bf8a2f80e1b24810b66dbc4ea4ab88a3addf1ef
Krylov Alexander <alkrev@gmail.com>
2025-05-28 09:55:08 +0300

    clean

--- EcpSigner.Application/Jobs/SignDocumentWorflow.cs
+++ EcpSigner.Application/Jobs/SignDocumentWorflow.cs
@@ -4,8 +4,6 @@ using EcpSigner.Domain.Interfaces;
 using EcpSigner.Domain.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
