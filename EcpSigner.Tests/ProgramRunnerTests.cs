06173208b808c4e715343fe8c751036603722b9e
Krylov Alexander <alkrev@gmail.com>
2025-05-29 16:24:19 +0300

    tests for EcpSigner project added

---
+++ EcpSigner.Tests/ProgramRunnerTests.cs
@@ -0,0 +1,86 @@
+﻿using EcpSigner.Application.Interfaces;
+using EcpSigner.Domain.Interfaces;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Moq;
+
+namespace EcpSigner
+{
+    public class ProgramRunnerTests
+    {
+        [Fact]
+        public async Task RunAsync_ShouldRunWorker_WithProvidedToken()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger>();
+            var workerMock = new Mock<IJob>();
+            var factoryMock = new Mock<IWorkerFactory>();
+            var cancelMock = new Mock<ICancellationService>();
+
+            var cts = new CancellationTokenSource();
+            cancelMock.SetupGet(c => c.Token).Returns(cts.Token);
+            cancelMock.Setup(c => c.StartListeningForCancel());
+
+            factoryMock.Setup(f => f.CreateWorker(It.IsAny<string[]>()))
+                       .Returns(workerMock.Object);
+
+            var runner = new ProgramRunner(loggerMock.Object, factoryMock.Object, cancelMock.Object);
+            var args = new[] { "test" };
+
+            // Act
+            await runner.RunAsync(args);
+
+            // Assert
+            cancelMock.Verify(c => c.StartListeningForCancel(), Times.Once);
+            factoryMock.Verify(f => f.CreateWorker(args), Times.Once);
+            workerMock.Verify(w => w.RunAsync(cts.Token), Times.Once);
+        }
+        [Fact]
+        public async Task RunAsync_ShouldRunWorker_WithoutProvidedToken()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger>();
+            var workerMock = new Mock<IJob>();
+            var factoryMock = new Mock<IWorkerFactory>();
+
+            factoryMock.Setup(f => f.CreateWorker(It.IsAny<string[]>()))
+                       .Returns(workerMock.Object);
+
+            var runner = new ProgramRunner(loggerMock.Object, factoryMock.Object);
+            var args = new[] { "test" };
+
+            // Act
+            await runner.RunAsync(args);
+
+            // Assert
+            factoryMock.Verify(f => f.CreateWorker(args), Times.Once);
+            workerMock.Verify(w => w.RunAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Run_ShouldLogFatal_WhenRunnerThrows()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger>();
+            var workerMock = new Mock<IJob>();
+            var factoryMock = new Mock<IWorkerFactory>();
+            var cancelMock = new Mock<ICancellationService>();
+
+            var cts = new CancellationTokenSource();
+            cancelMock.SetupGet(c => c.Token).Returns(cts.Token);
+            cancelMock.Setup(c => c.StartListeningForCancel());
+
+            factoryMock.Setup(f => f.CreateWorker(It.IsAny<string[]>()))
+                       .Returns(workerMock.Object);
+            workerMock.Setup(w => w.RunAsync(It.IsAny<CancellationToken>())).Throws(new Exception("failure"));
+
+            var runner = new ProgramRunner(loggerMock.Object, factoryMock.Object, cancelMock.Object);
+            var args = new[] { "test" };
+
+            // Act
+            await runner.RunAsync(args);
+
+            // Assert
+            loggerMock.Verify(l => l.Fatal(It.Is<string>(msg => msg.Contains("failure"))), Times.Once);
+        }
+    }
+}
---
+++ EcpSigner.Tests/ProgramTests.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+using EcpSigner;
+using EcpSigner.Domain.Interfaces;
+
+namespace EcpSigner
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Run_ShouldCallRunner_WhenRunnerIsProvided()
+        {
+            // Arrange
+            var args = new[] { "arg1", "arg2" };
+            var runnerMock = new Mock<IProgramRunner>();
+            runnerMock.Setup(r => r.RunAsync(args)).Returns(Task.CompletedTask);
+
+            // Act
+            Program.Run(args, runnerMock.Object);
+
+            // Assert
+            runnerMock.Verify(r => r.RunAsync(args), Times.Once);
+        }
+
+        [Fact]
+        public void Run_ShouldNotThrow_WhenRunnerThrows()
+        {
+            // Arrange
+            var args = new[] { "arg1" };
+            var runnerMock = new Mock<IProgramRunner>();
+            runnerMock.Setup(r => r.RunAsync(args))
+                      .ThrowsAsync(new Exception("Simulated failure"));
+
+            // Act
+            Action act = () => Program.Run(args, runnerMock.Object);
+
+            // Assert
+            act.Should().NotThrow(); // исключение должно быть перехвачено в Program.Run
+        }
+
+        [Fact]
+        public void Run_ShouldUseDefaultRunner_WhenNullPassed()
+        {
+            // Arrange
+            var args = new[] { "arg1" };
+
+            // Act
+            Action act = () => Program.Run(args, null);
+
+            // Assert
+            act.Should().NotThrow(); // должен создать ProgramRunner и выполнить без исключений
+        }
+    }
+}
+
