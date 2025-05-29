0f1ca49e644e66bcaf1ec4714657bcdcf6a25877
Krylov Alexander <alkrev@gmail.com>
2025-05-29 16:19:54 +0300

    added new CancellationService

---
+++ EcpSigner.Domain/Interfaces/ICancellationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EcpSigner.Domain.Interfaces
+{
+    public interface ICancellationService
+    {
+        CancellationToken Token { get; }
+        void StartListeningForCancel();
+    }
+}
---
+++ EcpSigner.Infrastructure/Services/ConsoleCancellationService .cs
@@ -0,0 +1,33 @@
+﻿using EcpSigner.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EcpSigner.Infrastructure.Services
+{
+    public class ConsoleCancellationService : ICancellationService
+    {
+        private readonly ILogger _logger;
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+
+        public ConsoleCancellationService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public CancellationToken Token => _cts.Token;
+
+        public void StartListeningForCancel()
+        {
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                e.Cancel = true;
+                _logger.Info("Ctrl-C нажато");
+                _cts.Cancel();
+            };
+        }
+    }
+}
