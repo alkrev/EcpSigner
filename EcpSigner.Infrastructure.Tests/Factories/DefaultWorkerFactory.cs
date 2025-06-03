27caefe68b2b6cdec53ccb76038589f8d540016a
Krylov Alexander <alkrev@gmail.com>
2025-06-03 13:27:35 +0300

    added factories tests

---
+++ EcpSigner.Infrastructure.Tests/Factories/DefaultWorkerFactory.cs
@@ -0,0 +1,75 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using EcpSigner.Application.Interfaces;
+using EcpSigner.Infrastructure.Factories;
+using EcpSigner.Infrastructure.Workers;
+using EcpSigner.Infrastructure.Services;
+using EcpSigner.Application.Jobs;
+using EcpSigner.Domain.Interfaces;
+
+namespace EcpSigner.Infrastructure.Factories
+{
+    public class DefaultWorkerFactoryTests
+    {
+        private readonly Mock<ILogger> _loggerMock = new();
+        private readonly Mock<IInfrastructureFactory> _infraFactoryMock = new();
+
+        private readonly Mock<IConfigurationProvider> _configMock = new();
+        private readonly Mock<IPortalService> _portalServiceMock = new();
+        private readonly Mock<ISignatureService> _signatureServiceMock = new();
+        private readonly Mock<ICacheService> _cacheServiceMock = new();
+        private readonly Mock<IDatesService> _datesServiceMock = new();
+        private readonly Mock<IFlashWindowService> _flashWindowServiceMock = new();
+        private readonly Mock<IDelayProvider> _delayProviderMock = new();
+        private readonly Mock<IDateTimeProvider> _dateTimeProviderMock = new();
+
+        private readonly string[] _args = new[] { "arg1", "arg2" };
+
+        public DefaultWorkerFactoryTests()
+        {
+            _infraFactoryMock.Setup(x => x.CreateConfigurationProvider()).Returns(_configMock.Object);
+            _infraFactoryMock.Setup(x => x.CreatePortalService()).Returns(_portalServiceMock.Object);
+            _infraFactoryMock.Setup(x => x.CreateSignatureService()).Returns(_signatureServiceMock.Object);
+            _infraFactoryMock.Setup(x => x.CreateCacheService()).Returns(_cacheServiceMock.Object);
+            _infraFactoryMock.Setup(x => x.CreateDatesService(It.IsAny<string[]>())).Returns(_datesServiceMock.Object);
+            _infraFactoryMock.Setup(x => x.CreateFlashWindowService()).Returns(_flashWindowServiceMock.Object);
+            _infraFactoryMock.Setup(x => x.CreateDelayProvider()).Returns(_delayProviderMock.Object);
+            _infraFactoryMock.Setup(x => x.CreateDateTimeProvider()).Returns(_dateTimeProviderMock.Object);
+        }
+
+        [Fact]
+        public void CreateWorker_ShouldReturnDocumentSigningWorker()
+        {
+            // Arrange
+            var factory = new DefaultWorkerFactory(_loggerMock.Object, _infraFactoryMock.Object);
+
+            // Act
+            var result = factory.CreateWorker(_args);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<DocumentSigningWorker>();
+        }
+
+        [Fact]
+        public void CreateWorker_ShouldCallAllFactoryMethods()
+        {
+            // Arrange
+            var factory = new DefaultWorkerFactory(_loggerMock.Object, _infraFactoryMock.Object);
+
+            // Act
+            factory.CreateWorker(_args);
+
+            // Assert
+            _infraFactoryMock.Verify(x => x.CreateConfigurationProvider(), Times.Once);
+            _infraFactoryMock.Verify(x => x.CreatePortalService(), Times.Once);
+            _infraFactoryMock.Verify(x => x.CreateSignatureService(), Times.Once);
+            _infraFactoryMock.Verify(x => x.CreateCacheService(), Times.Once);
+            _infraFactoryMock.Verify(x => x.CreateDatesService(_args), Times.Once);
+            _infraFactoryMock.Verify(x => x.CreateFlashWindowService(), Times.Once);
+            _infraFactoryMock.Verify(x => x.CreateDelayProvider(), Times.AtLeastOnce);
+            _infraFactoryMock.Verify(x => x.CreateDateTimeProvider(), Times.Once);
+        }
+    }
+}
---
+++ EcpSigner.Infrastructure.Tests/Factories/InfrastructureFactoryTests.cs
@@ -0,0 +1,161 @@
+﻿using CachingTools;
+using CAPICOM;
+using CryptographyTools.Signing;
+using CryptographyTools.Signing.CryptoPro;
+using CryptographyTools.Store;
+using Ecp.Portal;
+using Ecp.Web;
+using EcpSigner.Application.Interfaces;
+using EcpSigner.Domain.Interfaces;
+using EcpSigner.Infrastructure.Decorators;
+using EcpSigner.Infrastructure.Factories;
+using EcpSigner.Infrastructure.Services;
+using FluentAssertions;
+using Moq;
+using WindowsTools;
+using Xunit;
+
+namespace EcpSigner.Infrastructure.Factories
+{
+    public class InfrastructureFactoryTests
+    {
+        private readonly Mock<ILogger> _loggerMock = new();
+        private readonly Mock<IConfigurationProviderFactory> _configFactoryMock = new();
+        private readonly Mock<IWebClientFactory> _webClientFactoryMock = new();
+        private readonly Mock<ICryptoFactory> _cryptoFactoryMock = new();
+        private readonly Mock<IStoreFactory> _storeFactoryMock = new();
+        private readonly Mock<ICacheFactory> _cacheFactoryMock = new();
+        private readonly Mock<IFlashWindowFactory> _flashWindowFactoryMock = new();
+
+        private readonly InfrastructureFactory _factory;
+
+        public InfrastructureFactoryTests()
+        {
+            _factory = new InfrastructureFactory(
+                _loggerMock.Object,
+                _configFactoryMock.Object,
+                _webClientFactoryMock.Object,
+                _cryptoFactoryMock.Object,
+                _storeFactoryMock.Object,
+                _cacheFactoryMock.Object,
+                _flashWindowFactoryMock.Object
+            );
+        }
+
+        [Fact]
+        public void CreateConfigurationProvider_ShouldReturnInstance()
+        {
+            // Arrange
+            var configProviderMock = new Mock<IConfigurationProvider>();
+            _configFactoryMock.Setup(x => x.Create()).Returns(configProviderMock.Object);
+
+            // Act
+            var result = _factory.CreateConfigurationProvider();
+
+            // Assert
+            result.Should().BeSameAs(configProviderMock.Object);
+        }
+
+        [Fact]
+        public void CreatePortalService_ShouldReturnPortalServiceDecorator()
+        {
+            // Arrange
+            var configMock = new Mock<IConfigurationProvider>();
+            var configObject = new Domain.Models.AppSettings { url = "https://test.com", cacheMinutes = 10 };
+            configMock.Setup(x => x.Get()).Returns(configObject);
+            _configFactoryMock.Setup(x => x.Create()).Returns(configMock.Object);
+
+            var webClientMock = new Mock<IClient>();
+            _webClientFactoryMock.Setup(x => x.Create(configObject.url)).Returns(webClientMock.Object);
+
+            // Act
+            var result = _factory.CreatePortalService();
+
+            // Assert
+            result.Should().BeOfType<PortalServiceDecorator>();
+        }
+
+        [Fact]
+        public void CreateSignatureService_ShouldReturnSignatureServiceDecorator()
+        {
+            // Arrange
+            var cryptoMock = new Mock<ISigning>();
+            var storeMock = new Mock<ICurrentUserStore>();
+
+            _cryptoFactoryMock.Setup(x => x.Create()).Returns(cryptoMock.Object);
+            _storeFactoryMock.Setup(x => x.Create()).Returns(storeMock.Object);
+
+            // Act
+            var result = _factory.CreateSignatureService();
+
+            // Assert
+            result.Should().BeOfType<SignatureServiceDecorator>();
+        }
+
+        [Fact]
+        public void CreateCacheService_ShouldReturnCacheService()
+        {
+            // Arrange
+            var configMock = new Mock<IConfigurationProvider>();
+            var configObject = new Domain.Models.AppSettings { url = "url", cacheMinutes = 15 };
+            configMock.Setup(x => x.Get()).Returns(configObject);
+            _configFactoryMock.Setup(x => x.Create()).Returns(configMock.Object);
+
+            var cacheMock = new Mock<ICache>();
+            _cacheFactoryMock.Setup(x => x.Create(configObject.cacheMinutes)).Returns(cacheMock.Object);
+
+            // Act
+            var result = _factory.CreateCacheService();
+
+            // Assert
+            result.Should().BeOfType<CacheService>();
+        }
+
+        [Fact]
+        public void CreateDatesService_ShouldReturnDatesService()
+        {
+            // Arrange
+            var args = new[] { "arg1", "arg2" };
+
+            // Act
+            var result = _factory.CreateDatesService(args);
+
+            // Assert
+            result.Should().BeOfType<DatesService>();
+        }
+
+        [Fact]
+        public void CreateFlashWindowService_ShouldReturnFlashWindowService()
+        {
+            // Arrange
+            var flashMock = new Mock<IFlashWindow>();
+            _flashWindowFactoryMock.Setup(x => x.Create()).Returns(flashMock.Object);
+
+            // Act
+            var result = _factory.CreateFlashWindowService();
+
+            // Assert
+            result.Should().BeOfType<FlashWindowService>();
+        }
+
+        [Fact]
+        public void CreateDelayProvider_ShouldReturnDelayProvider()
+        {
+            // Act
+            var result = _factory.CreateDelayProvider();
+
+            // Assert
+            result.Should().BeOfType<DelayProvider>();
+        }
+
+        [Fact]
+        public void CreateDateTimeProvider_ShouldReturnDateTimeProvider()
+        {
+            // Act
+            var result = _factory.CreateDateTimeProvider();
+
+            // Assert
+            result.Should().BeOfType<DateTimeProvider>();
+        }
+    }
+}
