8d8e20deb53ddd855214a4fb0429af3e479379d8
Krylov Alexander <alkrev@gmail.com>
2025-06-05 16:25:14 +0300

    DateTimeProviderTests added

---
+++ EcpSigner.Infrastructure.Tests/Services/DateTimeProviderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EcpSigner.Domain.Interfaces;
+using EcpSigner.Infrastructure.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace EcpSigner.Infrastructure.Services
+{
+    public class DateTimeProviderTests
+    {
+        [Fact]
+        public void Now_ShouldReturnCurrentDateTime()
+        {
+            // Arrange
+            IDateTimeProvider provider = new DateTimeProvider();
+            var before = DateTime.Now;
+
+            // Act
+            var result = provider.Now;
+
+            var after = DateTime.Now;
+
+            // Assert
+            result.Should().BeOnOrAfter(before).And.BeOnOrBefore(after);
+        }
+    }
+}
