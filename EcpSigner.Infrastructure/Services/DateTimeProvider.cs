6e8ef7135c556fb19c39a90b3403f2ca53954593
Krylov Alexander <alkrev@gmail.com>
2025-06-05 16:16:29 +0300

    clean

--- EcpSigner.Infrastructure/Services/DateTimeProvider.cs
+++ EcpSigner.Infrastructure/Services/DateTimeProvider.cs
@@ -1,10 +1,5 @@
 ﻿using EcpSigner.Domain.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EcpSigner.Infrastructure.Services
 {
