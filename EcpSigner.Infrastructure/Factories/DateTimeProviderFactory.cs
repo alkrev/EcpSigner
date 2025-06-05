a8fe7ffa76f5ed8754ef1293e12cef411bf1b2a8
Krylov Alexander <alkrev@gmail.com>
2025-06-05 16:16:55 +0300

    DateTimeProviderFactory added

---
+++ EcpSigner.Infrastructure/Factories/DateTimeProviderFactory.cs
@@ -0,0 +1,18 @@
+﻿using EcpSigner.Domain.Interfaces;
+using EcpSigner.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcpSigner.Infrastructure.Factories
+{
+    public class DateTimeProviderFactory : IDateTimeProviderFactory
+    {
+        public IDateTimeProvider Create()
+        {
+            return new DateTimeProvider();
+        }
+    }
+}
---
+++ EcpSigner.Infrastructure/Factories/IDateTimeProviderFactory.cs
@@ -0,0 +1,15 @@
+﻿using EcpSigner.Domain.Interfaces;
+using EcpSigner.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcpSigner.Infrastructure.Factories
+{
+    public interface IDateTimeProviderFactory
+    {
+        IDateTimeProvider Create();
+    }
+}
