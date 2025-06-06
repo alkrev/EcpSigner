17d58428ec873b0b956be9ad56db852d66cc987e
Krylov Alexander <alkrev@gmail.com>
2025-06-06 11:47:22 +0300

    Cache refactored to use dateTimeProvider

--- EcpSigner.Infrastructure/Shared/CachingTools/Cache.cs
+++ EcpSigner.Infrastructure/Shared/CachingTools/Cache.cs
@@ -1,31 +1,37 @@
-﻿using System;
+﻿using EcpSigner.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CachingTools
 {
-    public class Cache: ICache
+    public class Cache : ICache
     {
-        private Dictionary<string, DateTime> cache;
-        private int minutes;
-        public Cache(int minutes)
+        private readonly Dictionary<string, DateTime> cache;
+        private readonly int minutes;
+        private readonly IDateTimeProvider dateTimeProvider;
+
+        public Cache(int minutes, IDateTimeProvider dateTimeProvider)
         {
             this.minutes = minutes;
+            this.dateTimeProvider = dateTimeProvider;
             cache = new Dictionary<string, DateTime>();
         }
+
         public void SetRange(List<string> numbers)
         {
-            DateTime now = DateTime.UtcNow.AddMinutes(minutes);
+            DateTime expirationTime = dateTimeProvider.Now.AddMinutes(minutes);
             foreach (string num in numbers)
             {
-                cache[num] = now;
+                cache[num] = expirationTime;
             }
         }
+
         public bool Contains(string number)
         {
             if (cache.TryGetValue(number, out DateTime expirationTime))
             {
-                if (expirationTime < DateTime.UtcNow)
+                if (expirationTime < dateTimeProvider.Now)
                 {
                     cache.Remove(number);
                     return false;
@@ -34,14 +40,20 @@ namespace CachingTools
             }
             return false;
         }
+
         public int Count()
         {
             return cache.Count;
         }
+
         public void RemoveExpired()
         {
-            DateTime now = DateTime.UtcNow;
-            var expiredKeys = cache.Where(entry => entry.Value < now).Select(entry => entry.Key).ToList();
+            DateTime now = dateTimeProvider.Now;
+            var expiredKeys = cache
+                .Where(entry => entry.Value < now)
+                .Select(entry => entry.Key)
+                .ToList();
+
             foreach (string key in expiredKeys)
             {
                 cache.Remove(key);
