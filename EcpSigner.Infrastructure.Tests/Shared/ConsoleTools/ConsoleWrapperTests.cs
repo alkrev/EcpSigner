bb99719819255b222c356f9d71ecb0ab9c9a5fa5
Krylov Alexander <alkrev@gmail.com>
2025-06-06 13:51:24 +0300

    ConsoleWrapperTests added

---
+++ EcpSigner.Infrastructure.Tests/Shared/ConsoleTools/ConsoleWrapperTests.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleTools;
+using FluentAssertions;
+using Moq;
+using System;
+using Xunit;
+
+namespace EcpSigner.Infrastructure.Shared.ConsoleTools
+{
+    public class ConsoleWrapperTests
+    {
+        [Fact]
+        public void AddingAndRemovingHandler_ShouldNotThrow()
+        {
+            // Arrange
+            var wrapper = new ConsoleWrapper();
+            ConsoleCancelEventHandler? handler = null;
+
+            // Act
+            Action act = () =>
+            {
+                wrapper.CancelKeyPress += handler;
+                wrapper.CancelKeyPress -= handler;
+            };
+
+            // Assert
+            act.Should().NotThrow("добавление и удаление обработчика через обёртку должно быть безопасным");
+        }
+    }
+}
