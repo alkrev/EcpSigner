3de90a7bad73f0a5991d08dfcb25dae9bd366874
Krylov Alexander <alkrev@gmail.com>
2025-06-08 09:42:53 +0300

    CurrentUserStoreTests added

---
+++ EcpSigner.Infrastructure.Tests/Shared/CryptographyTools/Store/CurrentUserStoreTests.cs
@@ -0,0 +1,32 @@
+﻿using CryptographyTools.Store;
+using FluentAssertions;
+
+namespace EcpSigner.Infrastructure.Shared.CryptographyTools.Store
+{
+    public class CurrentUserStoreTests
+    {
+        private readonly CurrentUserStore _store;
+
+        public CurrentUserStoreTests()
+        {
+            _store = new CurrentUserStore();
+        }
+
+        [Fact]
+        public void GetUserCertificates_ShouldReturnNonEmptyCollection_WhenCertificatesExist()
+        {
+            // Act
+            var certificates = _store.GetUserCertificates();
+
+            // Assert
+            certificates.Should().NotBeNull();
+            certificates.Count.Should().BeGreaterThan(0, "должен быть хотя бы один установленный сертификат");
+
+            foreach (var cert in certificates.Values)
+            {
+                cert.Subject.Should().NotBeNullOrEmpty();
+                cert.ValidToDate.Should().BeAfter(DateTime.MinValue);
+            }
+        }
+    }
+}
