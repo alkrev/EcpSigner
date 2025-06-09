11b05d58f4f98647d72e684b65b17cdd258d31dd
Krylov Alexander <alkrev@gmail.com>
2025-06-09 10:53:45 +0300

    DateTimeProviderFactoryTests added

---
+++ EcpSigner.Infrastructure.Tests/Factories/DateTimeProviderFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using CachingTools;
+using CryptographyTools.Signing.CryptoPro;
+using EcpSigner.Domain.Interfaces;
+using EcpSigner.Infrastructure.Services;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcpSigner.Infrastructure.Factories
+{
+    public class DateTimeProviderFactoryTests
+    {
+        [Fact]
+        public void Create_ShouldReturnDateTimeProviderFactoryInstance()
+        {
+            // Arrange
+            var factory = new DateTimeProviderFactory();
+
+            // Act
+            var result = factory.Create();
+
+            // Assert
+            result.Should().BeOfType<DateTimeProvider>();
+        }
+    }
+}
