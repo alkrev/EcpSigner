b16466363cdb5cac6dadddc972e49fa3834db729
Krylov Alexander <alkrev@gmail.com>
2025-06-09 08:50:50 +0300

    clean

--- EcpSigner.Infrastructure/Factories/IDateTimeProviderFactory.cs
+++ EcpSigner.Infrastructure/Factories/IDateTimeProviderFactory.cs
@@ -1,10 +1,4 @@
 ﻿using EcpSigner.Domain.Interfaces;
-using EcpSigner.Infrastructure.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EcpSigner.Infrastructure.Factories
 {
