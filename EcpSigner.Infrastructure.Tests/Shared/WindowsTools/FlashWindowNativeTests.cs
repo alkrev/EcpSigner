6c5542cfaabbae653df8970b4cea40b5b2d5e4c1
Krylov Alexander <alkrev@gmail.com>
2025-06-09 10:54:35 +0300

    FlashWindowTests added

---
+++ EcpSigner.Infrastructure.Tests/Shared/WindowsTools/FlashWindowNativeTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+namespace WindowsTools
+{
+    public class FlashWindowNativeTests
+    {
+        [Fact]
+        public void Flash_Should_Invoke_FlashWindowEx_And_Return_Bool()
+        {
+            // Arrange
+            var native = new FlashWindowNative();
+            var hwnd = IntPtr.Zero; // В реальности нужно передать корректный HWND окна
+            var fInfo = new FlashWindow.FLASHWINFO
+            {
+                cbSize = Convert.ToUInt32(System.Runtime.InteropServices.Marshal.SizeOf<FlashWindow.FLASHWINFO>()),
+                hwnd = hwnd,
+                dwFlags = FlashWindow.FLASHW_STOP,
+                uCount = 1,
+                dwTimeout = 0
+            };
+
+            // Act
+            Action act = () => native.Flash(ref fInfo);
+
+            // Assert
+            act.Should().NotThrow(); // Убедимся, что метод не выбрасывает исключений
+        }
+    }
+}
---
+++ EcpSigner.Infrastructure.Tests/Shared/WindowsTools/FlashWindowTests.cs
@@ -0,0 +1,69 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using System;
+using WindowsTools;
+
+namespace WindowsTools
+{
+    public class FlashWindowTests
+    {
+        private readonly IntPtr _dummyHwnd = new IntPtr(123456);
+
+        [Fact]
+        public void Start_Should_CallFlashWindowEx_WithCorrectParameters()
+        {
+            // Arrange
+            var nativeMock = new Mock<IFlashWindowNative>();
+
+            var flashWindow = new FlashWindow(_dummyHwnd, nativeMock.Object);
+
+            FlashWindow.FLASHWINFO capturedInfo = default;
+
+            nativeMock
+                .Setup(n => n.Flash(ref It.Ref<FlashWindow.FLASHWINFO>.IsAny))
+                .Returns(true)
+                .Callback((ref FlashWindow.FLASHWINFO info) => { capturedInfo = info; });
+
+            // Act
+            flashWindow.Start();
+
+            // Assert
+            capturedInfo.hwnd.Should().Be(_dummyHwnd);
+            capturedInfo.dwFlags.Should().Be(FlashWindow.FLASHW_ALL | FlashWindow.FLASHW_TIMERNOFG);
+            capturedInfo.uCount.Should().Be(UInt32.MaxValue);
+            capturedInfo.dwTimeout.Should().Be(0);
+            capturedInfo.cbSize.Should().Be(Convert.ToUInt32(System.Runtime.InteropServices.Marshal.SizeOf<FlashWindow.FLASHWINFO>()));
+
+            nativeMock.Verify(n => n.Flash(ref It.Ref<FlashWindow.FLASHWINFO>.IsAny), Times.Once);
+        }
+
+        [Fact]
+        public void Stop_Should_CallFlashWindowEx_WithCorrectParameters()
+        {
+            // Arrange
+            var nativeMock = new Mock<IFlashWindowNative>();
+
+            var flashWindow = new FlashWindow(_dummyHwnd, nativeMock.Object);
+
+            FlashWindow.FLASHWINFO capturedInfo = default;
+
+            nativeMock
+                .Setup(n => n.Flash(ref It.Ref<FlashWindow.FLASHWINFO>.IsAny))
+                .Returns(true)
+                .Callback((ref FlashWindow.FLASHWINFO info) => { capturedInfo = info; });
+
+            // Act
+            flashWindow.Stop();
+
+            // Assert
+            capturedInfo.hwnd.Should().Be(_dummyHwnd);
+            capturedInfo.dwFlags.Should().Be(FlashWindow.FLASHW_STOP);
+            capturedInfo.uCount.Should().Be(UInt32.MaxValue);
+            capturedInfo.dwTimeout.Should().Be(0);
+            capturedInfo.cbSize.Should().Be(Convert.ToUInt32(System.Runtime.InteropServices.Marshal.SizeOf<FlashWindow.FLASHWINFO>()));
+
+            nativeMock.Verify(n => n.Flash(ref It.Ref<FlashWindow.FLASHWINFO>.IsAny), Times.Once);
+        }
+    }
+}
