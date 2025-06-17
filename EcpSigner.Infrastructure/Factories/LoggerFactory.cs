01fd201fb889ae253a40f428e3d7b4823a9f76c2
Krylov Alexander <alkrev@gmail.com>
2025-06-17 10:02:45 +0300

    LoggerFactory class added

---
+++ EcpSigner.Infrastructure/Factories/LoggerFactory.cs
@@ -0,0 +1,14 @@
+﻿using EcpSigner.Domain.Interfaces;
+using EcpSigner.Infrastructure.Services;
+
+namespace EcpSigner.Infrastructure.Factories
+{
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger Create(string name)
+        {
+            var nlogLogger = NLog.LogManager.GetLogger(name);
+            return new NLogLogger(nlogLogger);
+        }
+    }
+}
