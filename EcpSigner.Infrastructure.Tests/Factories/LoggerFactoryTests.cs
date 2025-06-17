4c0523b12dd1963412839ba2d16941557fa7ae96
Krylov Alexander <alkrev@gmail.com>
2025-06-17 10:02:22 +0300

    LoggerFactoryTests integration test added

---
+++ EcpSigner.Infrastructure.Tests/Factories/LoggerFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using EcpSigner.Domain.Interfaces;
+using EcpSigner.Infrastructure.Services;
+using FluentAssertions;
+
+namespace EcpSigner.Infrastructure.Factories
+{
+    public class LoggerFactoryTests
+    {
+        [Fact]
+        public void Create_ShouldReturnNLogLogger_WithExpectedName()
+        {
+            // Arrange
+            var loggerName = "IntegrationTestLogger";
+            var factory = new LoggerFactory();
+
+            // Act
+            ILogger result = factory.Create(loggerName);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<NLogLogger>();
+        }
+    }
+}
